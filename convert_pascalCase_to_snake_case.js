769e899d6f08899488c554b3e5178a15ab40b919
SLIM <vintio234@gmail.com>
2024-12-03 20:06:16 +0200

    Solve the pascal_case challenge -

--- convert_pascalCase_to_snake_case.js
+++ convert_pascalCase_to_snake_case.js
@@ -1,22 +1,3 @@
-// function toUnderscore(string) {
-//   if (typeof string == "number") {
-//     return string.toString();
-//   }
-//   let result = "";
-//   for (let i = 0; i < string.length; i++) {
-//     // checking if the first character is uppercase and is not first character
-//     if (string[i] == string[i].toUpperCase() && string[i] != string[0]) {
-//       result += "_" + string[i].toLowerCase();
-//       // check for first character -> probably should come as the first check
-//     } else if (string[i] == string[0]) {
-//       result += string[i].toLowerCase();
-//     } else {
-//       result += string[i];
-//     }
-//   }
-//   return result;
-// }
-
 function toUnderscore(string) {
   if (typeof string == "number") {
     return string.toString();
@@ -27,7 +8,7 @@ function toUnderscore(string) {
       if (string[i] == string[0]) {
         result += string[i].toLowerCase();
       } else if (!Number.isNaN(Number(string[i]))) {
-        // so I convert the value from a string to a number, and then check if it's
+        // so I convert the value from a string to a number, and then check if it's a number
         // this is where I check for numbers and still do normal concatenation
         result += string[i];
       } else {
