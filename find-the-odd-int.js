813475155e3499062ec2d77deb3d463a52f8900f
SLIM <vintio234@gmail.com>
2024-12-12 10:07:19 +0200

    Solve for odd value --

---
+++ find-the-odd-int.js
@@ -0,0 +1,26 @@
+function findOdd(A) {
+  let resultObj = {};
+  let odd = "";
+  for (num of A) {
+    resultObj[num] = (resultObj[num] || 0) + 1;
+  }
+
+  for (key in resultObj) {
+    if (resultObj[key] % 2 !== 0) {
+      odd = key;
+    }
+  }
+
+  return Number(odd);
+}
+
+console.log(findOdd([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]));
+
+/*
+Examples
+[7] should return 7, because it occurs 1 time (which is odd).
+[0] should return 0, because it occurs 1 time (which is odd).
+[1,1,2] should return 2, because it occurs 1 time (which is odd).
+[0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+[1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+*/
